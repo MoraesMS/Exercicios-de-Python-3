55e22a50923f06006b6bd076f7c3644435233f2a
Marcelo Santana Moraes <113527419+MoraesMS@users.noreply.github.com>
2023-04-19 16:05:12 -0300

    Create 097_um_print_especial.py

---
+++ 097_um_print_especial.py
@@ -0,0 +1,10 @@
+def título(msg):
+    print('~' * (len(msg) + 4))
+    print(f'  {msg}')
+    print('~' * (len(msg) + 4))
+
+
+# programa principal
+título('Marcelo S Moraes')
+título('Curso de Python')
+título('CeV')
